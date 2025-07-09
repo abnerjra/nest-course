b04830a568b1bdbca3683b2765694bc0443baefd
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-08 22:07:19 -0600

    feat: actualizacion de modulos

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -4,6 +4,7 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -11,7 +12,8 @@ import { MongooseModule } from '@nestjs/mongoose';
       rootPath: join(__dirname, '..', 'public'),
     }),
     MongooseModule.forRoot('mongodb://localhost:27018/nest-pokedex'),
-    PokemonModule
+    PokemonModule,
+    CommonModule
   ],
 })
 export class AppModule { }
