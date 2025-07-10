cffe2dd4f2e19f9479312f6aa1782284c663582d
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-10 17:41:39 -0600

    feat: definición de base de datos

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -19,7 +19,9 @@ import { JoiValidationSchema } from './config/joi.valitation';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot(process.env.MONGO_DB!),
+    MongooseModule.forRoot(process.env.MONGO_DB!,{
+      dbName: 'pokemonsdb'
+    }),
     PokemonModule,
     CommonModule,
     SeedModule,
