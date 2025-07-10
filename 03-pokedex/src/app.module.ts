b97c245d066a946a37f4c0ee70b39d33e4f738db
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-09 20:24:35 -0600

    feat: configuración de variables de entorno

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,21 +1,30 @@
 import { join } from 'path';
-
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+
+import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27018/nest-pokedex'),
+    MongooseModule.forRoot(process.env.MONGO_DB!),
     PokemonModule,
     CommonModule,
-    SeedModule
+    SeedModule,
   ],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(configService: ConfigService) {
+    console.log('🔍 process.env.MONGO_DB:', process.env.MONGO_DB);
+    console.log('🔍 configService.get("MONGO_DB"):', configService.get('MONGO_DB'));
+  }
+}
