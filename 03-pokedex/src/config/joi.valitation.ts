3e522d799e4237795b7ae8de15e32967ff2d1995
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-10 20:36:14 -0600

    fix: corrección en mapeo de variables

--- 03-pokedex/src/config/joi.valitation.ts
+++ 03-pokedex/src/config/joi.valitation.ts
@@ -1,7 +1,7 @@
 import * as Joi from "joi";
 
 export const JoiValidationSchema = Joi.object({
-  PORT: Joi.required(),
-  MONGO_DB: Joi.string().default(3000),
+  PORT: Joi.required().default(3000),
+  MONGO_DB: Joi.string(),
   DEFAULT_LIMIT: Joi.number().default(6)
 });
