a7d4a512b97f74d284aeb1e7d63a01b981670306
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-11 15:29:33 -0600

    feat: modulo de productos

--- 04-teslo-shop/src/app.module.ts
+++ 04-teslo-shop/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
@@ -14,7 +15,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true // use dev
-    })
+    }),
+    ProductModule
   ],
 })
 export class AppModule { }
