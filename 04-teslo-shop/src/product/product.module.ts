60ad901e8e68a2ac4567f96390cf94ec453662a3
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-11 19:09:55 -0600

    feat: importación de CommonModule

--- 04-teslo-shop/src/product/product.module.ts
+++ 04-teslo-shop/src/product/product.module.ts
@@ -4,13 +4,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { Product } from './entities/product.entity';
+import { CommonModule } from 'src/common/common.module';
 
 
 @Module({
   controllers: [ProductController],
   providers: [ProductService],
   imports: [
-    TypeOrmModule.forFeature([Product])
+    TypeOrmModule.forFeature([Product]),
+    CommonModule
   ]
 })
 export class ProductModule { }
