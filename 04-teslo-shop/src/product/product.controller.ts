18cc8993ca9a788c4f3f77da13a4bd37b07ff4df
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-11 18:02:06 -0600

    feat: implementación de los metodos create, finAll, findOne y delete

--- 04-teslo-shop/src/product/product.controller.ts
+++ 04-teslo-shop/src/product/product.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseUUIDPipe
+} from '@nestjs/common';
+
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('product')
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+  constructor(private readonly productService: ProductService) { }
 
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
@@ -18,17 +28,17 @@ export class ProductController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id);
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.productService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto);
+    return this.productService.update(id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.productService.remove(id);
   }
 }
--- 04-teslo-shop/src/product/product.service.ts
+++ 04-teslo-shop/src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { CreateProductDto } from './dto/create-product.dto';
@@ -8,6 +8,8 @@ import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductService {
+
+  private readonly logger = new Logger('ProductService');
 
   constructor(
     @InjectRepository(Product)
@@ -22,24 +24,32 @@ export class ProductService {
       await this.productRepository.save(product);
       return product;
     } catch (error) {
-      console.log(error);
-      throw new InternalServerErrorException('Ayuda!!!')
+      this.handleDBExceptions(error);
     }
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async findAll() {
+    return this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product) throw new NotFoundException(`Product with id ${id} not found`);
+    return product
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  update(id: string, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    await this.productRepository.remove(product);
+  }
+
+  private handleDBExceptions(error: any) {
+    if (error.code === '23505') throw new BadRequestException(error.detail)
+    this.logger.error(error);
+    throw new InternalServerErrorException('Unexpected error, check server log')
   }
 }
