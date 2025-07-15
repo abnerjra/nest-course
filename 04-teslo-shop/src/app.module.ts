a86f43af500ee0fa06d3d41aad612dbebaef3efa
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-15 17:24:32 -0600

    feat: archivo actualizado

--- 04-teslo-shop/src/app.module.ts
+++ 04-teslo-shop/src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductModule } from './product/product.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { FileModule } from './file/file.module';
 
 @Module({
   imports: [
@@ -20,7 +21,8 @@ import { SeedModule } from './seed/seed.module';
     }),
     ProductModule,
     CommonModule,
-    SeedModule
+    SeedModule,
+    FileModule
   ],
 })
 export class AppModule { }
