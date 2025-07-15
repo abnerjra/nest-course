97012b4bc415080a563aabc2eff0cb1ad7ca9fd6
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-15 09:38:36 -0600

    feat: archivo actualizado

--- 04-teslo-shop/src/product/product.module.ts
+++ 04-teslo-shop/src/product/product.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { CommonModule } from 'src/common/common.module';
+
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
-import { Product } from './entities/product.entity';
-import { CommonModule } from 'src/common/common.module';
+import { Product, ProductImage } from './entities';
 
 
 @Module({
   controllers: [ProductController],
   providers: [ProductService],
   imports: [
-    TypeOrmModule.forFeature([Product]),
+    TypeOrmModule.forFeature([Product, ProductImage]),
     CommonModule
   ]
 })
