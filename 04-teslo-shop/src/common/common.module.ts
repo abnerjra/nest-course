8f2dd9372353d9e8c0eb8d5db160eafcef7622f5
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-16 12:29:22 -0600

    feat: definicion del nuevo adaptador

--- 04-teslo-shop/src/common/common.module.ts
+++ 04-teslo-shop/src/common/common.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UuidAdapter } from './adapters/uuid.adapter';
+import { BcryptAdapter } from './adapters/bcrypt.adapter';
 
 @Module({
-  providers: [UuidAdapter],
-  exports: [UuidAdapter]
+  providers: [
+    UuidAdapter,
+    BcryptAdapter
+  ],
+  exports: [
+    UuidAdapter,
+    BcryptAdapter
+  ]
 })
 export class CommonModule { }
