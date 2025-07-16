473607c3fe4d0293f8f055955983a151a74097f1
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-16 13:29:13 -0600

    feat: implementación de JWT

--- 04-teslo-shop/src/auth/auth.module.ts
+++ 04-teslo-shop/src/auth/auth.module.ts
@@ -1,19 +1,42 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { CommonModule } from 'src/common/common.module';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { User } from './entities/user.entity';
-import { CommonModule } from 'src/common/common.module';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
+    ConfigModule,
     TypeOrmModule.forFeature([User]),
-    CommonModule
+    CommonModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: { expiresIn: '1h' }
+        }
+      }
+    })
   ],
   exports: [
-    TypeOrmModule
+    TypeOrmModule,
+    JwtStrategy,
+    PassportModule,
+    JwtModule,
   ]
 })
 export class AuthModule { }
