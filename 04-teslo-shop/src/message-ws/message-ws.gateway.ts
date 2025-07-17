6f7df561f4bd249220fa4e69bcf70049c5e5e779
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-17 17:43:26 -0600

    feat: emitir evento hacia el cliente

--- 04-teslo-shop/src/message-ws/message-ws.gateway.ts
+++ 04-teslo-shop/src/message-ws/message-ws.gateway.ts
@@ -1,10 +1,17 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+  WebSocketServer
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
 import { MessageWsService } from './message-ws.service';
 
 @WebSocketGateway({ cors: true })
 export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() wsServer: Server
+
   constructor(
     private readonly messageWsService: MessageWsService
   ) { }
@@ -13,13 +20,19 @@ export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnec
     // console.log('Client connected: ', client.id);
     this.messageWsService.registerClient(client);
 
-    console.log({ clientsConnected: this.messageWsService.getConnectedClients() });
+    const clientsConnected = this.messageWsService.getConnectedClients();
+    // console.log({ clientsConnected: clientsConnected });
+    // emit -> enviar informacion al cliente
+    this.wsServer.emit('clients-updated', clientsConnected);
 
   }
 
   handleDisconnect(client: Socket) {
     // console.log('Client disconnected: ', client.id);
     this.messageWsService.removeClient(client.id);
-    console.log({ clientsConnected: this.messageWsService.getConnectedClients() });
+    // console.log({ clientsConnected: this.messageWsService.getConnectedClients() });
+    const clientsConnected = this.messageWsService.getConnectedClients();
+    // emit -> enviar informacion al cliente
+    this.wsServer.emit('clients-updated', clientsConnected);
   }
 }
