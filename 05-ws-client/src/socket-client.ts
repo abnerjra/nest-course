bdad163a1beeebd1229b4a484ec0245ea946f720
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-17 17:45:11 -0600

    feat: escuchar evento del servidor y mostrar clientes conectados en pantalla

--- 05-ws-client/src/socket-client.ts
+++ 05-ws-client/src/socket-client.ts
@@ -13,6 +13,7 @@ export const connectToServer = () => {
 
 const addListener = (socket: Socket) => {
   const serverStatusLabel = document.querySelector('#server-status');
+  const clientsUl = document.querySelector('#clients-ul');
 
   // Escuchar el servidor
   socket.on('connect', () => {
@@ -21,6 +22,12 @@ const addListener = (socket: Socket) => {
 
   socket.on('disconnect', () => {
     serverStatusLabel!.innerHTML = 'Offline';
+  });
+
+  socket.on('clients-updated', (clients: string[]) => {
+    let clientsHtml = '';
+    clients.forEach(clientId => clientsHtml += `<li>${clientId}</li>`);
+    clientsUl!.innerHTML = clientsHtml;
   });
 
   // Emitir un evento al servidor
