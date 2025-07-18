83ca887b5a5ce26f4212337acde709390de5689e
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-17 19:07:54 -0600

    feat: importacion de AuthModule

--- 04-teslo-shop/src/message-ws/message-ws.module.ts
+++ 04-teslo-shop/src/message-ws/message-ws.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+
+import { AuthModule } from 'src/auth/auth.module';
+
 import { MessageWsService } from './message-ws.service';
 import { MessageWsGateway } from './message-ws.gateway';
 
 @Module({
   providers: [MessageWsGateway, MessageWsService],
+  imports: [
+    AuthModule
+  ]
 })
-export class MessageWsModule {}
+export class MessageWsModule { }
