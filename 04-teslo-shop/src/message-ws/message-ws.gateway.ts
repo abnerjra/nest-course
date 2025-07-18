7d3b0e258de5f7e07b976ab9a8eae6cd1f876f54
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-17 18:07:21 -0600

    feat: escuchar eventos del cliente

--- 04-teslo-shop/src/message-ws/message-ws.gateway.ts
+++ 04-teslo-shop/src/message-ws/message-ws.gateway.ts
@@ -1,12 +1,14 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
 import { MessageWsService } from './message-ws.service';
+import { NewMessageDto } from './dtos';
 
 @WebSocketGateway({ cors: true })
 export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -35,4 +37,13 @@ export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnec
     // emit -> enviar informacion al cliente
     this.wsServer.emit('clients-updated', clientsConnected);
   }
+
+  //* Escuchar eventos del cliente
+  // message-client
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient(client: Socket, payload: NewMessageDto) {
+    console.log(client.id, payload);
+
+  }
+
 }
