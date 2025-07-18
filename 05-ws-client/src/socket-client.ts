36d17a65ad9c719ec682d9eb5851c6be1ea11200
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-17 18:57:15 -0600

    feat: enviar token hacie el servidor por medio de headers

--- 05-ws-client/src/socket-client.ts
+++ 05-ws-client/src/socket-client.ts
@@ -1,8 +1,13 @@
 import { Manager, Socket } from "socket.io-client";
 
-export const connectToServer = () => {
+export const connectToServer = (token: string) => {
 
-  const manager = new Manager('http://localhost:3000/socket.io/socket.io.js');
+  //! Enviar token mediante la propiedad extraHeaders
+  const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
+    extraHeaders: {
+      authentication: token
+    }
+  });
 
   const socket = manager.socket('/');
   // console.log({ socket });
