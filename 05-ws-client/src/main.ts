b2974e326aa4de51ec7c88f0551a1037e084fcfe
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-17 18:55:57 -0600

    feat: se añade campo para ingresar JWT y boton para enviar hacia el servidor

--- 05-ws-client/src/main.ts
+++ 05-ws-client/src/main.ts
@@ -3,7 +3,11 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Websocket - Client</h1>
+    <h2>Websocket - Client</h2>
+    <input id="jwt-client" placeholder="Token client...">
+    <button id="btn-connect">Connect</button>
+
+    <br/>
     <span id='server-status'>offline</span>
     <ol id="clients-ul"></ol>
 
@@ -17,4 +21,12 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `
 
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-connectToServer();
+// connectToServer();
+
+const jwtClient = document.querySelector<HTMLInputElement>('#jwt-client')!;
+const btnConnect = document.querySelector<HTMLButtonElement>('#btn-connect')!;
+
+btnConnect.addEventListener('click', () => {
+  if (jwtClient.value.trim().length <= 0) return alert('Enter a valid JWT');
+  connectToServer(jwtClient.value.trim());
+});
