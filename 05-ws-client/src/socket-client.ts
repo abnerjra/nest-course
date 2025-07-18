6955855b122dd6ed866f4f6e37cf35d74516cf6f
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-07-17 18:09:18 -0600

    feat: mandar evento hacia el servidor

--- 05-ws-client/src/socket-client.ts
+++ 05-ws-client/src/socket-client.ts
@@ -12,22 +12,35 @@ export const connectToServer = () => {
 }
 
 const addListener = (socket: Socket) => {
-  const serverStatusLabel = document.querySelector('#server-status');
-  const clientsUl = document.querySelector('#clients-ul');
+  const serverStatusLabel = document.querySelector('#server-status')!;
+  const clientsUl = document.querySelector('#clients-ul')!;
+
+  const messageForm = document.querySelector<HTMLFormElement>('#message-form')!;
+  const messageInput = document.querySelector<HTMLInputElement>('#message-input')!;
 
   // Escuchar el servidor
   socket.on('connect', () => {
-    serverStatusLabel!.innerHTML = 'Online';
+    serverStatusLabel.innerHTML = 'Online';
   });
 
   socket.on('disconnect', () => {
-    serverStatusLabel!.innerHTML = 'Offline';
+    serverStatusLabel.innerHTML = 'Offline';
   });
 
   socket.on('clients-updated', (clients: string[]) => {
     let clientsHtml = '';
     clients.forEach(clientId => clientsHtml += `<li>${clientId}</li>`);
-    clientsUl!.innerHTML = clientsHtml;
+    clientsUl.innerHTML = clientsHtml;
+  });
+
+  messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (messageInput.value.trim().length <= 0) return;
+
+    socket.emit('message-from-client', { id: 'Yo!!!', message: messageInput.value });
+    messageInput.value = '';
+
   });
 
   // Emitir un evento al servidor
